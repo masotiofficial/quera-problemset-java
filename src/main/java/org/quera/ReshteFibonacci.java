93871c39b4281582ce9bd73c110b872d47cb6910
Majid Soltanian <masotiofficial@gmail.com>
2025-03-01 02:43:31 +0330

    Reshte Fibonacci

---
+++ src/main/java/org/quera/ReshteFibonacci.java
@@ -0,0 +1,41 @@
+package org.quera;
+
+import java.util.Scanner;
+
+/*
+
+  Problem link: https://quera.org/problemset/17675
+
+*/
+
+public class ReshteFibonacci {
+    public static void main(String[] args) {
+        buildSString();
+    }
+
+    private static void buildSString() {
+        Input input = Input.getInput();
+
+        for (int i = 1; i <= input.n; i++) {
+            System.out.print(isFibonacci(i) ? '+' : '-');
+        }
+    }
+
+    private static boolean isFibonacci(int n) {
+        return isPerfectSquare(5 * n * n + 4) || isPerfectSquare(5 * n * n - 4);
+    }
+
+    private static boolean isPerfectSquare(int n) {
+        int sqrt = (int) Math.sqrt(n);
+
+        return sqrt * sqrt == n;
+    }
+
+    private record Input(int n) {
+        private static final Scanner SCANNER = new Scanner(System.in);
+
+        static Input getInput() {
+            return new Input(SCANNER.nextInt());
+        }
+    }
+}
