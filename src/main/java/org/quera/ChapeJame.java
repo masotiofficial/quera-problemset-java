3cad86020d5cbd2edc1733523633fd9e209dc6bc
Majid Soltanian <masotiofficial@gmail.com>
2025-02-28 18:12:45 +0330

    Chape Jame

---
+++ src/main/java/org/quera/ChapeJame.java
@@ -0,0 +1,45 @@
+package org.quera;
+
+import java.util.Scanner;
+
+/*
+
+  Problem link: https://quera.org/problemset/275795
+
+*/
+
+public class ChapeJame {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        printSumSequence(Input.getInput().n);
+    }
+
+    private static void printSumSequence(int n) {
+        System.out.println(buildSumSequence(n));
+    }
+
+    private static String buildSumSequence(int n) {
+        StringBuilder sequence = new StringBuilder();
+        int sum = calculateSum(n);
+
+        for (int i = 1; i < n; i++) {
+            sequence.append(i).append(" + ");
+        }
+        sequence.append(n).append(" = ").append(sum);
+
+        return sequence.toString();
+    }
+
+    private static int calculateSum(int n) {
+        return (n * (n + 1)) / 2;
+    }
+
+    private record Input(int n) {
+        private static final Scanner SCANNER = new Scanner(System.in);
+
+        static Input getInput() {
+            return new Input(SCANNER.nextInt());
+        }
+    }
+}
