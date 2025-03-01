1ba1613cd50d42bcad60c9ba5e3808c331d41883
Majid Soltanian <masotiofficial@gmail.com>
2025-03-01 03:32:52 +0330

    Ramin Va Masale Riazi

---
+++ src/main/java/org/quera/RaminVaMasaleRiazi.java
@@ -0,0 +1,39 @@
+package org.quera;
+
+import java.util.Scanner;
+
+/*
+
+  Problem link: https://quera.org/problemset/102261
+
+*/
+
+public class RaminVaMasaleRiazi {
+    public static void main(String[] args) {
+        Input.getInput();
+    }
+
+    private static int countPerfectSquareNumbers(int l, int r) {
+        return (int) (Math.floor(Math.sqrt(r)) - Math.ceil(Math.sqrt(l)) + 1);
+    }
+
+    private record Input() {
+        private static final Scanner SCANNER = new Scanner(System.in);
+
+        static void getInput() {
+            getNumberRange(SCANNER.nextInt());
+        }
+
+        private static void getNumberRange(int q) {
+            int l = 0;
+            int r = 0;
+
+            for (int i = 0; i < q; i++) {
+                l = SCANNER.nextInt();
+                r = SCANNER.nextInt();
+
+                System.out.println(countPerfectSquareNumbers(l, r));
+            }
+        }
+    }
+}
