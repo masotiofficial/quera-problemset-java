a55edf9e18d23861715aef9aac247f714a564add
Majid Soltanian <masotiofficial@gmail.com>
2025-03-01 08:32:18 +0330

    Yek Soal Sadeh

---
+++ src/main/java/org/quera/YekSoalSadeh.java
@@ -0,0 +1,27 @@
+package org.quera;
+
+import java.util.Scanner;
+
+/*
+
+  Problem link: https://quera.org/problemset/2885
+
+*/
+
+public class YekSoalSadeh {
+    public static void main(String[] args) {
+        repeat(Input.getInput().n);
+    }
+
+    private static void repeat(int n) {
+        System.out.println("man khoshghlab hastam\n".repeat(n));
+    }
+
+    private record Input(int n) {
+        private static final Scanner SCANNER = new Scanner(System.in);
+
+        static Input getInput() {
+            return new Input(SCANNER.nextInt());
+        }
+    }
+}
