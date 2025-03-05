da0ac7aecc239b977534fd277d8490e8b909d5f5
Majid Soltanian <masotiofficial@gmail.com>
2025-03-05 08:26:02 +0330

    Cube

---
+++ src/main/java/org/quera/Cube.java
@@ -0,0 +1,29 @@
+package org.quera;
+
+/*
+
+  Problem link: https://quera.org/problemset/33033
+
+*/
+
+public class Cube {
+    public int[][][] coloring(int[][][] cube) {
+        for (int i = 0; i < cube.length; i++) {
+            for (int j = 0; j < cube[i].length; j++) {
+                for (int k = 0; k < cube[i][j].length; k++) {
+                    if ((i == 0) || (i == cube.length - 1)) {
+                        cube[i][j][k] = 1;
+                    } else if ((j == 0) || (j == cube[i].length - 1)) {
+                        cube[i][j][k] = 1;
+                    } else if ((k == 0) || (k == cube[i][j].length - 1)) {
+                        cube[i][j][k] = 1;
+                    } else {
+                        cube[i][j][k] = 0;
+                    }
+                }
+            }
+        }
+
+        return cube;
+    }
+}
