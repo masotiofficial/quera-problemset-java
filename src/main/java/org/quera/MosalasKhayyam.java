5087f710d49c88822f0b18caac512971e89ddb02
Majid Soltanian <masotiofficial@gmail.com>
2025-03-06 19:17:41 +0330

    Mosalas Khayyam

---
+++ src/main/java/org/quera/MosalasKhayyam.java
@@ -0,0 +1,55 @@
+package org.quera;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+/*
+
+  Problem link: https://quera.org/problemset/595
+
+*/
+
+public class MosalasKhayyam {
+    public static void main(String[] args) {
+        process();
+    }
+
+    private static void process() {
+        IO input = IO.input();
+
+        Arrays.stream(khayyamPascalTriangle(input.n))
+                .map(row -> Arrays.stream(row)
+                        .mapToObj(String::valueOf)
+                        .collect(Collectors.joining(" ")))
+                .forEach(IO::output);
+    }
+
+    private static int[][] khayyamPascalTriangle(int n) {
+        int[][] triangle = new int[n][];
+
+        for (int i = 0; i < n; i++) {
+            triangle[i] = new int[i + 1];
+            triangle[i][0] = 1;
+            triangle[i][i] = 1;
+
+            for (int j = 1; j < i; j++) {
+                triangle[i][j] = triangle[i - 1][j] + triangle[i - 1][j - 1];
+            }
+        }
+
+        return triangle;
+    }
+
+    private record IO(int n) {
+        private static final Scanner SCANNER = new Scanner(System.in);
+
+        static IO input() {
+            return new IO(SCANNER.nextInt());
+        }
+
+        static void output(Object output) {
+            System.out.println(output);
+        }
+    }
+}
