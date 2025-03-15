5b10f275f7fdcdb7a72277a3d0535790ebd981ee
Majid Soltanian <masotiofficial@gmail.com>
2025-03-15 13:54:56 +0330

    Saba Va Soal Sadeh

---
+++ src/main/java/org/quera/SabaVaSoalSadeh.java
@@ -0,0 +1,37 @@
+package org.quera;
+
+import java.util.Scanner;
+
+/*
+
+  Problem link: https://quera.org/problemset/31025
+
+*/
+
+public class SabaVaSoalSadeh {
+    public static void main(String[] args) {
+        process();
+    }
+
+    private static void process() {
+        IO input = IO.input();
+
+        IO.output(divide(input.n, input.k));
+    }
+
+    private static int divide(int n, int k) {
+        return Math.floorDiv(n, (int) Math.pow(2, k));
+    }
+
+    private record IO(int n, int k) {
+        private static final Scanner SCANNER = new Scanner(System.in);
+
+        static IO input() {
+            return new IO(SCANNER.nextInt(), SCANNER.nextInt());
+        }
+
+        static void output(Object output) {
+            System.out.println(output);
+        }
+    }
+}
