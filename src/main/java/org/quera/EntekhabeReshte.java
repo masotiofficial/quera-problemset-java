842ced36d249366bec01119c3a7deb3b4c91f4a1
Majid Soltanian <masotiofficial@gmail.com>
2025-03-15 13:28:12 +0330

    Entekhabe Reshte

---
+++ src/main/java/org/quera/EntekhabeReshte.java
@@ -0,0 +1,37 @@
+package org.quera;
+
+import java.util.Scanner;
+
+/*
+
+  Problem link: https://quera.org/problemset/182276
+
+*/
+
+public class EntekhabeReshte {
+    public static void main(String[] args) {
+        process();
+    }
+
+    private static void process() {
+        IO input = IO.input();
+
+        IO.output(isPriority(input.location) ? "YES" : "NO");
+    }
+
+    private static boolean isPriority(String location) {
+        return location.equals("EAST");
+    }
+
+    private record IO(String location) {
+        private static final Scanner SCANNER = new Scanner(System.in);
+
+        static IO input() {
+            return new IO(SCANNER.nextLine());
+        }
+
+        static void output(Object output) {
+            System.out.println(output);
+        }
+    }
+}
