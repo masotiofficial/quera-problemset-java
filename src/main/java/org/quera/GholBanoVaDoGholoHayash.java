4fa1cbf6f0196be098f7b9baae56a225df32ef08
Majid Soltanian <masotiofficial@gmail.com>
2025-03-16 18:44:54 +0330

    Ghol Bano Va Do Gholo Hayash

---
+++ src/main/java/org/quera/GholBanoVaDoGholoHayash.java
@@ -0,0 +1,37 @@
+package org.quera;
+
+import java.util.Scanner;
+
+/*
+
+  Problem link: https://quera.org/problemset/72876
+
+*/
+
+public class GholBanoVaDoGholoHayash {
+    public static void main(String[] args) {
+        process();
+    }
+
+    private static void process() {
+        IO input = IO.input();
+
+        IO.output(reverse(input.name));
+    }
+
+    private static String reverse(String name) {
+       return new StringBuilder(name).reverse().toString();
+    }
+
+    private record IO(String name) {
+        private static final Scanner SCANNER = new Scanner(System.in);
+
+        static IO input() {
+            return new IO(SCANNER.nextLine());
+        }
+
+        static void output(Object output) {
+            System.out.print(output);
+        }
+    }
+}
