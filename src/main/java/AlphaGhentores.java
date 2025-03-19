487851186921c76f44ebc3f347dc82e4efc7ec5c
Majid Soltanian <masotiofficial@gmail.com>
2025-03-19 13:03:00 +0330

    Alpha Ghentores

---
+++ src/main/java/AlphaGhentores.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/*
+
+  Problem link: https://quera.org/problemset/66859
+
+*/
+
+public class AlphaGhentores {
+    public static void main(String[] args) {
+        process();
+    }
+
+    private static void process() {
+        IO input = IO.input();
+
+        IO.output(convertToBase(input.n, input.b));
+    }
+
+    private static String convertToBase(int number, int base) {
+        return Integer.toUnsignedString(number, base).toUpperCase();
+    }
+
+    private record IO(int n, int b) {
+        private static final Scanner SCANNER = new Scanner(System.in);
+
+        static IO input() {
+            return new IO(SCANNER.nextInt(), SCANNER.nextInt());
+        }
+
+        static void output(Object output) {
+            System.out.println(output);
+        }
+    }
+}
