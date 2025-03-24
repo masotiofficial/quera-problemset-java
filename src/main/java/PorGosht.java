acfc2b5d9a4830aebaa4c0abaf5b0e5a154bf9ed
Majid Soltanian <masotiofficial@gmail.com>
2025-03-24 18:42:39 +0330

    Por Gosht

---
+++ src/main/java/PorGosht.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+
+  Problem link: https://quera.org/problemset/72882
+
+*/
+
+public class PorGosht {
+    public static void main(String[] args) {
+        process();
+    }
+
+    private static void process() {
+        IO input = IO.input();
+
+        IO.output(countMeats(input.firstContainer) + " " + countMeats(input.secondContainer));
+    }
+
+    private static long countMeats(String[] container) {
+        return Arrays.stream(container)
+                .mapToLong(row -> row.chars()
+                        .filter(ch -> ch == '*')
+                        .count())
+                .sum();
+    }
+
+    private record IO(String[] firstContainer, String[] secondContainer) {
+        private static final Scanner SCANNER = new Scanner(System.in);
+
+        static IO input() {
+            int n = SCANNER.nextInt();
+            int m = SCANNER.nextInt();
+            SCANNER.nextLine();
+
+            String[] firstContainer = new String[n];
+            for (int i = 0; i < n; i++) {
+                firstContainer[i] = SCANNER.nextLine();
+            }
+
+            String[] secondContainer = new String[n];
+            for (int i = 0; i < n; i++) {
+                secondContainer[i] = SCANNER.nextLine();
+            }
+
+            return new IO(firstContainer, secondContainer);
+        }
+
+        static void output(Object output) {
+            System.out.println(output);
+        }
+    }
+}
