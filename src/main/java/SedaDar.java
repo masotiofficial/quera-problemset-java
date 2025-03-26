5485445cbf7bcd8f564bea6bdca8ad2e664754ee
Majid Soltanian <masotiofficial@gmail.com>
2025-03-26 17:25:38 +0330

    Seda Dar

---
+++ src/main/java/SedaDar.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/*
+
+  Problem link: https://quera.org/problemset/140033
+
+*/
+
+public class SedaDar {
+    public static void main(String[] args) {
+        process();
+    }
+
+    private static void process() {
+        IO input = IO.input();
+
+        IO.output(countVowels(input.string));
+    }
+
+    private static int countVowels(String string) {
+        return string.replaceAll("[^aeiou]", "").length();
+    }
+
+    private record IO(String string) {
+        private static final Scanner SCANNER = new Scanner(System.in);
+
+        static IO input() {
+            return new IO(SCANNER.nextLine());
+        }
+
+        static void output(Object output) {
+            System.out.println(output);
+        }
+    }
+}
